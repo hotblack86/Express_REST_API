54c96232a2cf7942032f950af541eec798a6bcdf
James Hotblack <james.c.hotblack@gmail.com>
2019-07-16 13:43:28 +0100

    Set up routes, models, and mongo. Can now make succesful post requests to the DB.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+
+app.use(bodyParser.json());
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute)
+
 
 // Routes
 app.get('/', (req,res) => {
   res.send('Home Page!');
 })
 
-app.get('/posts', (req,res) => {
-  res.send('Posts Page!');
-})
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true }, () =>
+  console.log('Connected to DB!')
+);
 
 // Listen to server
 app.listen(3000);
